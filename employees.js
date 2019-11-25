d2c444fa32f3924fd7cdcd6dd03361bb6ecd792e
Your Name <alexsigala87@gmail.com>
2019-11-24 17:50:31 -0800

    created fucntion which takes preset emplyees aka manager decision for particular employees to be at certain stations and a json of all the employees. created if statement that runs certain code if presets are defined otherwise algo will decide all employees. began working on presets half othe algo. it has a switch case which will update a certain prperty of the workrotation object  depending on the station value of the preset

--- employees.js
+++ employees.js
@@ -29,26 +29,54 @@ let workers = [
     },
     {
         name: ["new", "person"],
-        certStations: ["i", "h"],
+        certStations: ["i", "h", "j", "g"],
         currentStation: "i",
         daysSinceLastTrained: 0,
         qualityIssues: 0,
         daysSinceLastQualityIssue: 0,
         buildEfficiency: [["", ""]]
     },
-    {
-        name: ["old", "vet"],
-        certStations: ["i", "h", "g", "f"],
-        currentStation: "",
-        daysSinceLastTrained: 0,
-        qualityIssues: 0,
-        daysSinceLastQualityIssue: 0,
-        buildEfficiency: [["", ""]]
-    },
+
     {
         name: ["other", "person"],
-        certStations: ["j", "k", "l"],
-        currentStation: "",
+        certStations: ["j", "k", "l", "i"],
+        currentStation: "k",
+        daysSinceLastTrained: 0,
+        qualityIssues: 0,
+        daysSinceLastQualityIssue: 0,
+        buildEfficiency: [["", ""]]
+    },
+    {
+        name: ["tan", "phan"],
+        certStations: ["k", "j"],
+        currentStation: "j",
+        daysSinceLastTrained: 20,
+        qualityIssues: 4,
+        daysSinceLastQualityIssue: 30,
+        buildEfficiency: [["", ""]]
+    },
+    {
+        name: ["new", "guy"],
+        certStations: ["g", "f", "e", "h"],
+        currentStation: "h",
+        daysSinceLastTrained: 0,
+        qualityIssues: 0,
+        daysSinceLastQualityIssue: 0,
+        buildEfficiency: [["", ""]]
+    },
+    {
+        name: ["nick", "munoz"],
+        certStations: ["g", "h", "f", "e", "i"],
+        currentStation: "g",
+        daysSinceLastTrained: 0,
+        qualityIssues: 0,
+        daysSinceLastQualityIssue: 0,
+        buildEfficiency: [["", ""]]
+    },
+    {
+        name: ["duyn", "nguyen"],
+        certStations: ["f", "e", "d", "g", "h"],
+        currentStation: "f",
         daysSinceLastTrained: 0,
         qualityIssues: 0,
         daysSinceLastQualityIssue: 0,
@@ -56,53 +84,44 @@ let workers = [
     },
     {
         name: ["", ""],
-        certStations: [""],
-        currentStation: "",
+        certStations: ["e", "f", "d", "c"],
+        currentStation: "e",
         daysSinceLastTrained: 0,
         qualityIssues: 0,
         daysSinceLastQualityIssue: 0,
         buildEfficiency: [["", ""]]
     },
     {
-        name: ["", ""],
-        certStations: [""],
-        currentStation: "",
+        name: ["richard", "guy"],
+        certStations: ["d", "c", "b", "e", "f"],
+        currentStation: "d",
         daysSinceLastTrained: 0,
         qualityIssues: 0,
         daysSinceLastQualityIssue: 0,
         buildEfficiency: [["", ""]]
     },
     {
-        name: ["", ""],
-        certStations: [""],
-        currentStation: "",
+        name: ["third", "man"],
+        certStations: ["c", "b", "d", "e"],
+        currentStation: "c",
         daysSinceLastTrained: 0,
         qualityIssues: 0,
         daysSinceLastQualityIssue: 0,
         buildEfficiency: [["", ""]]
     },
     {
-        name: ["", ""],
-        certStations: [""],
-        currentStation: "",
+        name: ["second", "dude"],
+        certStations: ["a", "c", "d", "b"],
+        currentStation: "b",
         daysSinceLastTrained: 0,
         qualityIssues: 0,
         daysSinceLastQualityIssue: 0,
         buildEfficiency: [["", ""]]
     },
     {
-        name: ["", ""],
-        certStations: [""],
-        currentStation: "",
-        daysSinceLastTrained: 0,
-        qualityIssues: 0,
-        daysSinceLastQualityIssue: 0,
-        buildEfficiency: [["", ""]]
-    },
-    {
-        name: ["", ""],
-        certStations: [""],
-        currentStation: "",
+        name: ["first", "guy"],
+        certStations: ["a", "b", "c", "d"],
+        currentStation: "a",
         daysSinceLastTrained: 0,
         qualityIssues: 0,
         daysSinceLastQualityIssue: 0,
--- scheduler.js
+++ scheduler.js
@@ -1,4 +1,81 @@
 let employees = require("./employees");
-
-function setSchedule(employees) {}
+//define some presets to make sure they do not change after running set schedule
+let presets = [
+    [["alex", "Sigala"], "m"],
+    [["Rosi", "Calderone"], "k"]
+];
+function setSchedule(employees, presets) {
+    console.log("setting new rotation");
+    console.log(`emplyess to be pre assigned are ${presets}`);
+    //define the workstation object
+    let workRotation = {
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+        e: "",
+        f: "",
+        g: "",
+        h: "",
+        i: "",
+        j: "",
+        k: "",
+        l: "",
+        m: ""
+    };
+    //if the manager wants a particular person somwehre set it
+    if (presets) {
+        console.log("assigning emplyuees to preset stations");
+        presets.forEach(employee => {
+            let employeeName = employee[0];
+            let assignedStation = employee[1];
+            //set up a switch case to determine which prperty to update
+            switch (assignedStation) {
+                case "a":
+                    break;
+                case "b":
+                    break;
+                case "c":
+                    break;
+                case "d":
+                    break;
+                case "e":
+                    break;
+                case "f":
+                    break;
+                case "g":
+                    break;
+                case "h":
+                    break;
+                case "i":
+                    break;
+                case "j":
+                    break;
+                case "k":
+                    break;
+                case "l":
+                    break;
+                case "m":
+                    break;
+            }
+            console.log(`
+            \n station a is assigned employee ${workRotation.a}
+            \n station b is assigned employee ${workRotation.b}
+            \n station c is assigned employee ${workRotation.c}
+            \n station d is assigned employee ${workRotation.b}
+            \n station e is assigned employee ${workRotation.e}
+            \n station f is assigned employee ${workRotation.f}
+            \n station g is assigned employee ${workRotation.g}
+            \n station h is assigned employee ${workRotation.h}
+            \n station i is assigned employee ${workRotation.i}
+            \n station j is assigned employee ${workRotation.j}
+            \n station k is assigned employee ${workRotation.k}
+            \n station l is assigned employee ${workRotation.l}
+            \n station m is assigned employee ${workRotation.m}`);
+        });
+    } else {
+        return "no presets";
+    }
+}
 console.log(`employees are ${employees}`);
+setSchedule(employees, presets);
